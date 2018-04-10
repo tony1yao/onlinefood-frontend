15e65e6975f62ba82c2a632dbf81243bed0bd8e4
Tony Yao <yao.tony@outlook.com>
2018-04-10 17:05:29 +1000

    Refined the style of the header part, added background image and three icons for the shop brand, promotions and bulletin.

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,8 +17,18 @@
                 <span class="text">{{seller.supports[0].description}}</span>
             </div>
         </div>
+        <div v-if="seller.supports" class="support-count">
+            <span class="count">{{seller.supports.length}} items</span>
+            <i class="icon-keyboard_arrow_right"></i>
+        </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+        <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+        <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+        <img :src="seller.avatar" height="100%" width="100%">
+    </div>
 </div>
 </template>
 
@@ -97,4 +107,55 @@ export default {
             .text
                 line-height 12px
                 font-size 10px
+      .support-count
+          position absolute
+          right 12px
+          bottom 14px
+          padding 0 8px
+          height 24px
+          line-height 24px
+          border-radius 14px
+          background-color rgba(0,0,0,0.2)
+          text-align center
+          .count
+              font-size 10px
+          .icon-keyboard_arrow_right
+              font-size 10px
+              line-height 24px
+              margin-left 2px
+    .bulletin-wrapper
+        height 28px
+        line-height 28px
+        padding 0 22px 0 12px
+        white-space nowrap
+        overflow hidden
+        text-overflow ellipsis
+        position relative
+        background-color rgba(7,17,27,0.2)
+        .bulletin-title
+            width 22px
+            height 12px
+            display inline-block
+            vertical-align top
+            margin-top 7px
+            bg-image('bulletin')
+            background-size 22px 12px
+            background-repeat no-repeat
+        .bulletin-text
+            font-size 10px
+            margin 0 4px
+            vertical-align top
+        .icon-keyboard_arrow_right
+            position absolute
+            font-size 10px
+            right 12px
+            top 8px
+    .background
+        position absolute
+        top 0
+        left 0
+        width 100%
+        height 100%
+        z-index -1
+        filter blur(10px)
 </style>
