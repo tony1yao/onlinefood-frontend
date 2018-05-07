a0371c98895a074a4af7e710975879e5f9233fa0
Tony Yao <yao.tony@outlook.com>
2018-05-07 23:03:53 +1000

    Completed the details information layer and learnt to use Flex layout.

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -36,9 +36,28 @@
                 <div class="star-wrapper">
                     <star :size="48" :score="seller.score"></star>
                 </div>
+                <div class="title">
+                    <div class="line"></div>
+                    <div class="text">Promotions</div>
+                    <div class="line"></div>
+                </div>
+                <ul class="supports" v-if="seller.supports">
+                    <li class="support-item" v-for="(item,index) in seller.supports" :key="index">
+                        <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                        <span class="text">{{seller.supports[index].description}}</span>
+                    </li>
+                </ul>
+                <div class="title">
+                    <div class="line"></div>
+                    <div class="text">Notice</div>
+                    <div class="line"></div>
+                </div>
+                <div class="bulletin">
+                    <p class="content">{{seller.bulletin}}</p>
+                </div>
             </div>
         </div>
-        <div class="detail-close">
+        <div class="detail-close" @click="closeDetail">
             <i class="icon-close"></i>
         </div>
     </div>
@@ -63,6 +82,9 @@ export default {
       showDetail() {
           console.log("inside of showDetail()");
           this.detailShow = true;
+      },
+      closeDetail() {
+          this.detailShow = false;
       }
   },
   components: {
@@ -210,6 +232,56 @@ export default {
                     margin-top 18px
                     padding 2px
                     text-align center
+                .title
+                    display flex
+                    width 80%
+                    margin 28px auto 24px auto
+                    .line
+                        flex 1
+                        position relative
+                        top -6px
+                        border-bottom 1px solid rgba(255, 255, 255, 0.2)
+                    .text
+                        padding 0 12px
+                        font-weight 700
+                        font-size 14px
+                .supports
+                    width 80%
+                    margin 0 auto
+                    .support-item
+                        padding 0 12px
+                        margin-bottom 12px
+                        font-size 0
+                        &:last-child
+                            margin-bottom 0
+                        .icon
+                            display inline-block
+                            width 16px
+                            height 16px
+                            vertical-align top
+                            margin-right 6px
+                            background-size 16px 16px
+                            background-repeat no-repeat
+                            &.decrease
+                                bg-image("decrease_2")
+                            &.discount
+                                bg-image("discount_2")
+                            &.guarantee
+                                bg-image("guarantee_2")
+                            &.invoice
+                                bg-image("invoice_2")
+                            &.special
+                                bg-image("special_2")
+                        .text
+                            line-height 12px
+                            font-size 13px
+                .bulletin
+                    width 80%
+                    margin 0 auto 
+                    .content
+                        padding 0 12px
+                        line-height 24px
+                        font-size 13px
 
         .detail-close
             position relative
