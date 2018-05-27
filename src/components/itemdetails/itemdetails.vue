3d6be3f14b2f022a77956d1fa566609ef08dea46
Tony <yao.tony@outlook.com>
2018-05-27 16:53:51 +1000

    added detailed page for items

---
+++ src/components/itemdetails/itemdetails.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-show="showFlag" class="itemdetails"></div>
+</template>
+<script>
+export default {
+    props: {
+        item: {
+            type: Object
+        }
+    },
+    method: {
+        show() {
+            this.showFlag = true;
+        }
+    },
+    data() {
+        return {
+            showFlag: false
+        }
+    }
+}
+</script>
+<style lang="stylus">
+    .itemdetails
+        position fixed
+        left 0
+        top 0
+        bottom 48px
+        z-index 30
+        width 100%
+        background #ffffff
+
+</style>
