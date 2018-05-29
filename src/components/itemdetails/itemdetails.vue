0e9e7115c640c0aa4b7166bb33c062bae466dcc9
Tony <yao.tony@outlook.com>
2018-05-29 23:02:49 +1000

    fine-tuned the style of item details page.

--- src/components/itemdetails/itemdetails.vue
+++ src/components/itemdetails/itemdetails.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <transition name="move">
-            <div v-show="showFlag" class="item">
+            <div v-show="showFlag" class="item" ref="root">
                 <div class="item-content">
                     <div class="image-header">
                         <img :src="item.image" alt="The picture of the food">
@@ -12,13 +12,19 @@
                     <div class="content">
                         <h1 class="header">{{item.name}}</h1>
                         <div class="details">
-                            <span class="sell-count">{{item.sellCount}} Sold</span>
+                            <span class="sell-count">{{item.sellCount}} Sold      </span>
                             <span class="rating">Good ratings: {{item.rating}}%</span>
                         </div>
+                        <div class="price">
+                            <span class="currentprice">${{item.price}}</span>
+                            <span v-show="item.oldPrice" class="oldprice">{{item.oldPrice}}</span>
+                        </div>
                     </div>
-                    <div class="price">
-                        <span class="currentprice">${{item.price}}</span>
-                        <span v-show="item.oldPrice" class="oldprice">{{item.oldPrice}}</span>
+                    <div class="cartcontroller-wrapper">
+                        <cartcontroller :item="item"></cartcontroller>
+                    </div>
+                    <div @click="addFirstItem" class="buy" v-show="!item.count || item.count===0">
+                        Add to cart
                     </div>
                 </div>
             </div>
@@ -26,6 +32,10 @@
     </div>
 </template>
 <script>
+import BScroll from 'better-scroll';
+import cartcontroller from 'components/cartcontroller/cartcontroller';
+import Vue from 'vue';
+
 export default {
     props: {
         item: {
@@ -35,15 +45,33 @@ export default {
     methods: {
         show() {
             this.showFlag = true;
+            this.$nextTick(()=>{
+                if(!this.scroll) {
+                    this.scroll = new BScroll(this.$refs.root,{
+                        click: true
+                    })
+                }else {
+                    this.scroll.refresh();
+                }
+            });
         },
         goBack() {
             this.showFlag = false;
+        },
+        addFirstItem(event) {
+            if(!event._constructed) {
+                return;
+            }
+            Vue.set(this.item, 'count', 1)   //set属性的值
         }
     },
     data() {
         return {
             showFlag: false
         }
+    },
+    components: {
+        cartcontroller
     }
 }
 </script>
@@ -80,4 +108,50 @@ export default {
                     padding 10px //这里的padding是为了增加按钮的响应区域面积
                     font-size 20
                     color #ffffff
+        .content
+            padding 18px
+            .header
+                line-height 14px
+                margin-bottom 8px
+                font-size 14px
+                font-weight 700
+                color rgb(7,17,27)
+            .details
+                margin-bottom 18px
+                line-height 10px
+                font-size 0
+                height 10px
+                .sell-count, .rating
+                    font-size 10px
+                    color rgb(147,153,159)
+                .sell-count
+                    margin-left 12px
+            .price
+                font-weight 700
+                line-height 24px
+                .currentprice
+                    margin-right 8px
+                    font-size 14px
+                    color rgb(240, 20, 20)
+                .oldprice
+                    text-decoration line-through
+                    font-size 10px
+                    color rgb(147,153,159)
+        .cartcontroller-wrapper
+            position absolute
+            right 12px
+            bottom 12px
+        .buy
+            position absolute
+            right 18px
+            bottom 18px
+            z-index 10
+            height 24px
+            line-height 24px
+            padding 0 12px
+            box-sizing border-box
+            font-size 10px
+            border-radius 12px
+            color #ffffff
+            background-color rgb(0,160,220)
 </style>
